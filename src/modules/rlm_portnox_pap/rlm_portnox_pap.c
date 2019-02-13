cade80503e40a40705d42659cab283cecc508a94
Vasiliy Kravchuk <vaskravchuk@gmail.com>
2019-02-13 19:06:51 +0200

    f

--- src/modules/rlm_portnox_pap/rlm_portnox_pap.c
+++ src/modules/rlm_portnox_pap/rlm_portnox_pap.c
@@ -36,7 +36,7 @@ static void plain_pwd_processor(dstr* value);
 static int portnox_pap_auth(void *instance, REQUEST *request)
 {
     static auth_attr_proc_t procs[1] = { (auth_attr_proc_t){USER_PWD_ATTR, PLAIN_PWD_PR, NULL} };
-    static auth_attr_proc_list_t proc_list = {procs, sizeof(procs)}
+    static auth_attr_proc_list_t proc_list = {procs, sizeof(procs)};
 
 	int result = NULL;
 	VALUE_PAIR *answer = NULL;
