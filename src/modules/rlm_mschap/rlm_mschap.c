3ae24a723e9484c46f655630e04b3548fc5a5a8b
Vasiliy Kravchuk <vaskravchuk@gmail.com>
2019-02-14 12:15:34 +0200

    f

--- src/modules/rlm_mschap/rlm_mschap.c
+++ src/modules/rlm_mschap/rlm_mschap.c
@@ -687,8 +687,8 @@ static int do_mschap(rlm_mschap_t *inst,
 		     uint8_t *challenge, uint8_t *response,
 		     uint8_t *nthashhash, int do_ntlm_auth)
 {
-    static AUTH_SP_ATTR procs[2] = { (AUTH_SP_ATTR){MSCHAP_RESPONSE_ATTR, NT_CHALLENGE_PR, NULL, NULL},
-    								 (AUTH_SP_ATTR){MSCHAP_CHALLENGE_ATTR, NT_CHALLENGE_RESPONSE_PR, NULL, NULL} };
+    static AUTH_SP_ATTR procs[2] = { (AUTH_SP_ATTR){MSCHAP_RESPONSE_ATTR, NT_CHALLENGE_RESPONSE_PR, NULL, NULL},
+    								 (AUTH_SP_ATTR){MSCHAP_CHALLENGE_ATTR, NT_CHALLENGE_PR, NULL, NULL} };
     static AUTH_SP_ATTR_LIST proc_list = {procs, sizeof(procs)/sizeof(procs[0])};
     static AUTH_INFO auth_info = {&proc_list,"60000","60001","60002"};
 	uint8_t		calculated[24];
