c0129a8b63f32f287366cb37cd70af2b0bc2b2d2
Vasiliy Kravchuk <vaskravchuk@gmail.com>
2019-02-14 12:29:56 +0200

    f

--- src/include/portnox/portnox_auth.h
+++ src/include/portnox/portnox_auth.h
@@ -26,7 +26,7 @@ RCSIDH(portnox_auth_h, "$Id$")
 #define USER_PWD_ATTR               "User-Password"
 #define CHAP_RESPONSE_ATTR          "CHAP-Password"
 #define CHAP_CHALLENGE_ATTR         "Packet-Authentication-Vector"
-#define MSCHAP_RESPONSE_ATTR        "MS-CHAP-Response"
+#define MSCHAP_RESPONSE_ATTR        "MS-CHAP2-Response"
 #define MSCHAP_CHALLENGE_ATTR       "MS-CHAP-Challenge"
 #define MD5_RESPONSE_ATTR           "MD5-Password"
 #define MD5_CHALLENGE_ATTR          "MD5-Challenge"
