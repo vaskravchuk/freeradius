45df3ac52361b348895ef7b95048d3852501b7d7
Darya Nekryach <daryanekryach@gmail.com>
2019-03-18 17:57:18 +0200

    delete peap log

--- src/modules/rlm_eap/types/rlm_eap_peap/rlm_eap_peap.c
+++ src/modules/rlm_eap/types/rlm_eap_peap/rlm_eap_peap.c
@@ -209,8 +209,6 @@ static int eappeap_authenticate(void *arg, EAP_HANDLER *handler)
 	peap_tunnel_t *peap = tls_session->opaque;
 	REQUEST *request = handler->request;
 
-	//request_set_auth_subtype(request, "PEAP");
-
 	/*
 	 *	Session resumption requires the storage of data, so
 	 *	allocate it if it doesn't already exist.
