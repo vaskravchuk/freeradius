59d954dea8ba852658cafab52867ba829a601738
Darya Nekryach <daryanekryach@gmail.com>
2019-04-02 15:47:20 +0300

    fix

--- src/modules/rlm_eap/types/rlm_eap_tls/rlm_eap_tls.c
+++ src/modules/rlm_eap/types/rlm_eap_tls/rlm_eap_tls.c
@@ -1680,7 +1680,7 @@ static int eaptls_authenticate(void *arg, EAP_HANDLER *handler)
 				}
 
 				char *error_id = ssl_error_to_error_id(handler->ssl_error);
-				char description = strcmp(error_id, "60040") == 0 ? handler->ssl_error_desc : NULL;
+				char *description = strcmp(error_id, "60040") == 0 ? handler->ssl_error_desc : NULL;
 
 				radius_exec_logger_centrale(handler->request, error_id, description);
 			}
