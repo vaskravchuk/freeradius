2ffa4553d04da6e7c24b3bd3c4237c7019436005
Yevhenii Nikitin <yevhenii.nikitin@developex.local>
2019-05-21 19:01:32 +0300

    Freeradius crashes fix

--- src/modules/rlm_eap/libeap/cb.c
+++ src/modules/rlm_eap/libeap/cb.c
@@ -108,17 +108,17 @@ void handler_set_ssl_error(EAP_HANDLER *handler, char *error, char *description)
 	/*memcpy(handler->ssl_error, error_id, sizeof(error_id));
 	memcpy(handler->ssl_error_desc, description, sizeof(description));*/
 
-	snprintf(handler->ssl_error, sizeof(error_id), "%s", error_id);
-	snprintf(handler->ssl_error_desc, sizeof(handler->ssl_error_desc), "%s\0", description);
+	/*snprintf(handler->ssl_error, sizeof(error_id), "%s", error_id);
+	snprintf(handler->ssl_error_desc, sizeof(handler->ssl_error_desc), "%s", description);*/
 
-	/*int len_error = snprintf(handler->ssl_error, sizeof(handler->ssl_error), "%s", error_id);
+	int len_error = snprintf(handler->ssl_error, sizeof(handler->ssl_error), "%s", error_id);
 	int len_desc = snprintf(handler->ssl_error_desc, sizeof(handler->ssl_error_desc), "%s", description);
 
-	len_error = MIN(len_error, sizeof(handler->ssl_error) - 1) < 0 ? 0 : len_error;
-	len_desc = MIN(len_desc, sizeof(handler->ssl_error_desc) - 1) < 0 ? 0 : len_desc;
+	len_error = len_error > sizeof(handler->ssl_error) - 1 ? sizeof(handler->ssl_error) - 1 : len_error;
+	len_desc = len_desc > sizeof(handler->ssl_error_desc) - 1 ? sizeof(handler->ssl_error_desc) - 1 : len_desc;
 
 	handler->ssl_error[len_error] = 0;
-	handler->ssl_error_desc[len_desc] = 0;		*/
+	handler->ssl_error_desc[len_desc] = 0;
 }
 
 void cbtls_info(const SSL *s, int where, int ret)
